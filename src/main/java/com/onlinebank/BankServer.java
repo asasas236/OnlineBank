df2d0d19dab1ace5e760146f7fbeacae4b23ac7a
asasas236 <asasas234@126.com>
2013-08-18 22:00:15 +0800

    sssssssssssssssss

--- src/main/java/com/onlinebank/BankServer.java
+++ src/main/java/com/onlinebank/BankServer.java
@@ -2,13 +2,13 @@ package com.onlinebank;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-import com.onlinebank.command.Command;
+import com.onlinebank.command.ICommand;
 
 public class BankServer {
 		
 	private static ConcurrentHashMap<String, Account> accountMap = new ConcurrentHashMap<>();
 	
-	public boolean services(String id,Command command){
+	public boolean services(String id,ICommand command){
 		accountMap.putIfAbsent(id, new Account(id));
 		return command.excuteCommand(accountMap.get(id));
 	}
